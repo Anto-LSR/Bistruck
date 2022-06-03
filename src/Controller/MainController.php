6775122963af0c3d6b41f8227a24422f89c31bcf
Anto-LSR <anton1.lesueur@gmail.com>
2022-06-03 23:44:47 +0200

    [BACKOFFICE] création du twig layout et fonctionnalité edition accueil

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,8 +2,13 @@
 
 namespace App\Controller;
 
+use App\Repository\AccueilRepository;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Event\ResponseEvent;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MainController extends AbstractController
@@ -11,10 +16,61 @@ class MainController extends AbstractController
     /**
      * @Route("/", name="app_main")
      */
-    public function index(): Response
+    public function index(AccueilRepository $ar): Response
     {
+        $accueil = $ar->find(array('id' => '1'));
+
         return $this->render('main/index.html.twig', [
             'controller_name' => 'MainController',
+            'accueil' => $accueil
+        ]);
+    }
+
+    /**
+     * @Route("/rfadmin", name="app_adminPanel")
+     */
+    public function adminIndex(): Response
+    {
+        //TODO        GERER CONNEXION ADMIN
+        return $this->redirectToRoute('app_adminPanel_accueil');
+    }
+
+
+    /**
+     * @Route("/rfadmin/accueil", name="app_adminPanel_accueil")
+     */
+    public function adminAccueil(Request $request, AccueilRepository $ar, EntityManagerInterface $em): Response {
+        //Gestion des requêtes POST
+        if(!empty($_POST)){
+            //On récupère l'entité accueil
+            $accueil = $ar->find(array('id' => '1'));
+            //On récupère les valeurs du formulaire
+            $titre = $_POST["titreAccueil"];
+            $description = $_POST["descAccueil"];
+
+            //On applique le style au mot clé Rock & Foulk
+            if (str_contains($titre, 'Rock & Foulk')){
+                $titleArray = explode("Rock & Foulk", $titre);
+                $titleArray[0] .= "<span id='hero-title'>Rock & Foulk</span>";
+                $titre = implode($titleArray);
+            }
+            //on hydrate les attributs d'accueil
+            $accueil->setTitre($titre);
+            $accueil->setDescription($description);
+            //on update et flush
+            $em->persist($accueil);
+            $em->flush();
+        }
+        //On enlève les balises HTML à afficher dans le form
+        $accueil = $ar->find(array('id' => '1'));
+        $accueilTitre = str_replace("<span id='hero-title'>", "",$accueil->getTitre());
+        $accueilTitre = str_replace("</span>", "",$accueilTitre);
+
+
+        //on rend la vue
+        return $this->render('admin/accueil.html.twig', [
+            'accueil' => $accueil,
+            'accueilTitre' => $accueilTitre
         ]);
     }
 }
---
+++ src/Repository/AccueilRepository.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Accueil;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\Exception\ORMException;
+use Doctrine\ORM\OptimisticLockException;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<Accueil>
+ *
+ * @method Accueil|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Accueil|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Accueil[]    findAll()
+ * @method Accueil[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class AccueilRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Accueil::class);
+    }
+
+    /**
+     * @throws ORMException
+     * @throws OptimisticLockException
+     */
+    public function add(Accueil $entity, bool $flush = false): void
+    {
+        $this->_em->persist($entity);
+        if ($flush) {
+            $this->_em->flush();
+        }
+    }
+
+    /**
+     * @throws ORMException
+     * @throws OptimisticLockException
+     */
+    public function remove(Accueil $entity, bool $flush = false): void
+    {
+        $this->_em->remove($entity);
+        if ($flush) {
+            $this->_em->flush();
+        }
+    }
+
+//    /**
+//     * @return Accueil[] Returns an array of Accueil objects
+//     */
+//    public function findByExampleField($value): array
+//    {
+//        return $this->createQueryBuilder('a')
+//            ->andWhere('a.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->orderBy('a.id', 'ASC')
+//            ->setMaxResults(10)
+//            ->getQuery()
+//            ->getResult()
+//        ;
+//    }
+
+//    public function findOneBySomeField($value): ?Accueil
+//    {
+//        return $this->createQueryBuilder('a')
+//            ->andWhere('a.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->getQuery()
+//            ->getOneOrNullResult()
+//        ;
+//    }
+}
